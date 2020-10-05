ca8ab943bb08d80dbb01f24e9f3774eb6be5d5cb
sekjohn <sujohn478@gmail.com>
2020-10-05 18:25:02 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
+
+const actions = {
+  init(initialValue) {  
+    return {value : initialValue}
+  },
+  increment(prevState) {
+    return {value : prevState.value + 1 };
+  },
+  decrement(prevState){
+    return {value : prevState.value - 1 };
+  }
+
+}
+
+
+class Counter1 extends React.Component {
+  state = actions.init(this.props.initialValue)
+  render() {
+    const { value }= this.state;
+    return (
+      <div>
+        Counter1:{value}
+        <Button onClick={() => this.setState(actions.increment)}>증가</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>감소</Button>
+      </div>
+    )
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Counter1 initialValue ={0} />
     </div>
   );
 }
